bc41cd952fb1551d1990d2b0470a92b97f73679b
Jiri Simacek <isimacek@fit.vutbr.cz>
2012-01-11 17:55:42 +0100

    explicit_tree_incl_down.cc: experimental implementation of non-recursive downward inclusion

--- include/vata/explicit_tree_aut_op.hh
+++ include/vata/explicit_tree_aut_op.hh
@@ -19,6 +19,7 @@
 #include <vata/explicit_tree_useless.hh>
 #include <vata/explicit_tree_unreach.hh>
 #include <vata/explicit_tree_transl.hh>
+#include <vata/explicit_tree_incl_down.hh>
 #include <vata/explicit_tree_incl_up.hh>
 #include <vata/down_tree_incl_fctor.hh>
 #include <vata/down_tree_opt_incl_fctor.hh>
@@ -246,9 +247,11 @@ namespace VATA {
 	template <class SymbolType, class Rel>
 	bool CheckDownwardInclusionWithPreorder(const ExplicitTreeAut<SymbolType>& smaller,
 		const ExplicitTreeAut<SymbolType>& bigger, const Rel& preorder) {
-
+/*
 		return CheckDownwardTreeInclusion<ExplicitTreeAut<SymbolType>,
 			VATA::DownwardInclusionFunctor>(smaller, bigger, preorder);
+*/
+		return ExplicitDownwardInclusion::Check(smaller, bigger, preorder);
 
 	}
 
